3cc9d962b7c98aabcc9d627ee27c54a4fd9cb73f
john <johnsmith2020@yahoo.com>
2017-03-02 07:26:55 +0700

    init

---
+++ src/main/java/testmvpro/testmvpro/App.java
@@ -0,0 +1,13 @@
+package testmvpro.testmvpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
